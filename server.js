28282265dfd1e59816efc975e36cbe924a0fc30b
pakawaka1 <davidcervantesdev@gmail.com>
2020-02-07 11:39:00 -0800

    initial push with package.json, server.js

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+
+const errorHandler = require('./middleware/error');
+const connectDB = require('./config/db');
+
+// Load env variables
+dotenv.config({ path: './config/config.env' });
+
+// Connect to DB
+connectDB();
+
+// Route files
+const cars = require('./routes/cars');
+const trucks = require('./routes/trucks');
+const boats = require('./routes/boats');
+const auth = require('./routes/auth');
+
+const app = express();
+
+app.use(express.json());
+
+//middleware
+app.use(cookieParser());
+app.use(errorHandler);
+
+app.use('/cars', cars);
+app.use('/trucks', trucks);
+app.use('/boats', boats);
+app.use('/auth', auth);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
+
+process.on('unhandledRejection', (err, promise) => {
+  console.log(err);
+  console.log(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
+});
